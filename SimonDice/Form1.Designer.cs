1041c95caeadf6daba339e0f2a17974779f9c468
JiV84 <Spunish@yahoo.es>
2015-11-05 08:49:52 +0100

    Primer Commit, proyecto v1.0 mejorando

---
+++ SimonDice/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SimonDice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lbl_Nivel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 142);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(185, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 142);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(12, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 142);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Goldenrod;
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(186, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 141);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // start
+            // 
+            this.start.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start.Location = new System.Drawing.Point(86, 341);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(93, 40);
+            this.start.TabIndex = 4;
+            this.start.Text = "Empezar";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(185, 341);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Comprobar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lbl_Nivel
+            // 
+            this.lbl_Nivel.AutoSize = true;
+            this.lbl_Nivel.Font = new System.Drawing.Font("Sawasdee", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nivel.Location = new System.Drawing.Point(149, 9);
+            this.lbl_Nivel.Name = "lbl_Nivel";
+            this.lbl_Nivel.Size = new System.Drawing.Size(67, 24);
+            this.lbl_Nivel.TabIndex = 6;
+            this.lbl_Nivel.Text = "Nivel 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(364, 393);
+            this.Controls.Add(this.lbl_Nivel);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon Says...";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lbl_Nivel;
+    }
+}
+
---
+++ SimonDice/Form1.cs
@@ -0,0 +1,162 @@
+﻿// //  Copyright (c) 2015 José Luis del Pino Gallardo
+// //  Nombre: SimonSays
+// //  Versión: 1.0.1
+// //  Fecha: 04/11/2015
+// //  This program is free software: you can redistribute it and/or modify
+// //  it under the terms of the GNU Lesser General Public License as published by
+// //  the Free Software Foundation, either version 3 of the License, or
+// //  (at your option) any later version.
+// //
+// //  This program is distributed in the hope that it will be useful,
+// //  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// //  GNU Lesser General Public License for more details.
+// //
+// //  You should have received a copy of the GNU Lesser General Public License
+// //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SimonDice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string cadena;
+        public string usuario;
+        int retardo = 500;
+        int dificultad = 5;
+        int level = 1;
+
+        private void Generar()
+        {
+            cadena = string.Empty;
+            usuario = string.Empty;
+
+            lbl_Nivel.Text = "Nivel: " + level;
+            start.Enabled = false;
+            Random rnd = new Random();
+            int num = 0;
+
+            for (int i = 0; i < dificultad ; i++)
+		    {
+                num = rnd.Next(0, 4);
+                cadena += num;
+
+                switch (num)
+                {
+                    case 0:
+                        Colorear(Color.DarkRed, Color.LightSalmon, button1);
+                        break;
+                    case 1:
+                        Colorear(Color.DarkGreen, Color.LightGreen, button2);
+                        break;
+                    case 2:
+                        Colorear(Color.MidnightBlue, Color.LightBlue, button3);
+                        break;
+                    case 3:
+                        Colorear(Color.Gold, Color.Khaki, button4);
+                        break;
+                }
+		    }
+
+            Acertar();
+        }
+
+        private void Colorear(Color origen, Color nuevo, Button nBut)
+        {
+            Thread.Sleep(100);
+            nBut.BackColor = nuevo;
+            Application.DoEvents();
+            Thread.Sleep(retardo);
+            nBut.BackColor = origen;
+            Application.DoEvents();
+        }
+
+        private void Acertar()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void DesactivarBot()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            usuario += 0;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            usuario += 1;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            usuario += 2;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            usuario += 3;
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            Generar();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DesactivarBot();
+            if (cadena == usuario)
+            {
+                MessageBox.Show("Has acertado! Enhorabuena");
+                start.Enabled = true;
+                button5.Enabled = false;
+                dificultad++;
+                retardo -= 50;
+                level++;
+
+            }
+            else
+            {
+                MessageBox.Show("Has fallado! Inténtalo otra vez");
+                start.Enabled = true;
+                button5.Enabled = false;
+                if (level > 1)
+                {
+                    dificultad--;
+                    retardo += 50;
+                    level--;
+                }
+                               
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Programa creado por José Luis del Pino Gallardo \nV1.0.1");
+        }
+    }
+}
