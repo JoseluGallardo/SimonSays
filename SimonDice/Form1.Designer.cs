3ce407009a7066ba7754340cea195fe5aa913bec
JiV84 <Spunish@yahoo.es>
2015-11-05 09:53:18 +0100

    v1.2 Juego terminado, ayuda añadida, faltan comentarios

--- SimonDice/Form1.Designer.cs
+++ SimonDice/Form1.Designer.cs
@@ -36,6 +36,14 @@
             this.start = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.lbl_Nivel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.juegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reiniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propiedadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -43,7 +51,7 @@
             this.button1.BackColor = System.Drawing.Color.DarkRed;
             this.button1.Enabled = false;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(12, 45);
+            this.button1.Location = new System.Drawing.Point(12, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 142);
             this.button1.TabIndex = 0;
@@ -55,7 +63,7 @@
             this.button2.BackColor = System.Drawing.Color.DarkGreen;
             this.button2.Enabled = false;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(185, 46);
+            this.button2.Location = new System.Drawing.Point(185, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(167, 142);
             this.button2.TabIndex = 1;
@@ -67,7 +75,7 @@
             this.button3.BackColor = System.Drawing.Color.MidnightBlue;
             this.button3.Enabled = false;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(12, 193);
+            this.button3.Location = new System.Drawing.Point(12, 224);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(167, 142);
             this.button3.TabIndex = 2;
@@ -79,7 +87,7 @@
             this.button4.BackColor = System.Drawing.Color.Goldenrod;
             this.button4.Enabled = false;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(186, 194);
+            this.button4.Location = new System.Drawing.Point(186, 225);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(166, 141);
             this.button4.TabIndex = 3;
@@ -90,18 +98,18 @@
             // 
             this.start.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.start.Location = new System.Drawing.Point(86, 341);
+            this.start.Location = new System.Drawing.Point(86, 372);
             this.start.Name = "start";
             this.start.Size = new System.Drawing.Size(93, 40);
             this.start.TabIndex = 4;
-            this.start.Text = "Empezar";
+            this.start.Text = "Start";
             this.start.UseVisualStyleBackColor = true;
             this.start.Click += new System.EventHandler(this.start_Click);
             // 
             // button5
             // 
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(185, 341);
+            this.button5.Location = new System.Drawing.Point(185, 372);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(93, 40);
             this.button5.TabIndex = 5;
@@ -113,18 +121,78 @@
             // 
             this.lbl_Nivel.AutoSize = true;
             this.lbl_Nivel.Font = new System.Drawing.Font("Sawasdee", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Nivel.Location = new System.Drawing.Point(149, 9);
+            this.lbl_Nivel.Location = new System.Drawing.Point(149, 40);
             this.lbl_Nivel.Name = "lbl_Nivel";
             this.lbl_Nivel.Size = new System.Drawing.Size(67, 24);
             this.lbl_Nivel.TabIndex = 6;
             this.lbl_Nivel.Text = "Nivel 1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.Font = new System.Drawing.Font("Sawasdee", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.juegoToolStripMenuItem,
+            this.propiedadesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(364, 26);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // juegoToolStripMenuItem
+            // 
+            this.juegoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reiniciarToolStripMenuItem});
+            this.juegoToolStripMenuItem.Name = "juegoToolStripMenuItem";
+            this.juegoToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.juegoToolStripMenuItem.Text = "Juego";
+            // 
+            // reiniciarToolStripMenuItem
+            // 
+            this.reiniciarToolStripMenuItem.Name = "reiniciarToolStripMenuItem";
+            this.reiniciarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.reiniciarToolStripMenuItem.Text = "Reiniciar";
+            this.reiniciarToolStripMenuItem.Click += new System.EventHandler(this.reiniciarToolStripMenuItem_Click);
+            // 
+            // propiedadesToolStripMenuItem
+            // 
+            this.propiedadesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.propiedadesToolStripMenuItem.Name = "propiedadesToolStripMenuItem";
+            this.propiedadesToolStripMenuItem.Size = new System.Drawing.Size(87, 22);
+            this.propiedadesToolStripMenuItem.Text = "Propiedades";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca De...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PapayaWhip;
-            this.ClientSize = new System.Drawing.Size(364, 393);
+            this.ClientSize = new System.Drawing.Size(364, 422);
             this.Controls.Add(this.lbl_Nivel);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.start);
@@ -132,12 +200,16 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Simon Says...";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,6 +224,13 @@
         private System.Windows.Forms.Button start;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label lbl_Nivel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem juegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reiniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propiedadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
     }
 }
 
--- SimonDice/Form1.cs
+++ SimonDice/Form1.cs
@@ -1,7 +1,7 @@
 ﻿// //  Copyright (c) 2015 José Luis del Pino Gallardo
 // //  Nombre: SimonSays
-// //  Versión: 1.0.1
-// //  Fecha: 04/11/2015
+// //  Versión: 1.2
+// //  Fecha: 05/11/2015
 // //  This program is free software: you can redistribute it and/or modify
 // //  it under the terms of the GNU Lesser General Public License as published by
 // //  the Free Software Foundation, either version 3 of the License, or
@@ -28,135 +28,165 @@ namespace SimonDice
             InitializeComponent();
         }
 
-        public string cadena;
-        public string usuario;
-        int retardo = 500;
-        int dificultad = 5;
-        int level = 1;
+        #region Campos
+            public string cadena;
+            public string usuario;
+            int retardo = 500;
+            int dificultad = 5;
+            int level = 1;
+        #endregion
 
+        #region Métodos
         private void Generar()
-        {
-            cadena = string.Empty;
-            usuario = string.Empty;
-
-            lbl_Nivel.Text = "Nivel: " + level;
-            start.Enabled = false;
-            Random rnd = new Random();
-            int num = 0;
-
-            for (int i = 0; i < dificultad ; i++)
-		    {
-                num = rnd.Next(0, 4);
-                cadena += num;
-
-                switch (num)
-                {
-                    case 0:
-                        Colorear(Color.DarkRed, Color.LightSalmon, button1);
-                        break;
-                    case 1:
-                        Colorear(Color.DarkGreen, Color.LightGreen, button2);
-                        break;
-                    case 2:
-                        Colorear(Color.MidnightBlue, Color.LightBlue, button3);
-                        break;
-                    case 3:
-                        Colorear(Color.Gold, Color.Khaki, button4);
-                        break;
-                }
-		    }
-
-            Acertar();
-        }
-
-        private void Colorear(Color origen, Color nuevo, Button nBut)
-        {
-            Thread.Sleep(100);
-            nBut.BackColor = nuevo;
-            Application.DoEvents();
-            Thread.Sleep(retardo);
-            nBut.BackColor = origen;
-            Application.DoEvents();
-        }
-
-        private void Acertar()
-        {
-            button1.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = true;
-            button4.Enabled = true;
-            button5.Enabled = true;
-        }
-
-        private void DesactivarBot()
-        {
-            button1.Enabled = false;
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            usuario += 0;
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            usuario += 1;
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            usuario += 2;
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            usuario += 3;
-
-        }
-
-        private void start_Click(object sender, EventArgs e)
-        {
-            Generar();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            DesactivarBot();
-            if (cadena == usuario)
             {
-                MessageBox.Show("Has acertado! Enhorabuena");
-                start.Enabled = true;
-                button5.Enabled = false;
-                dificultad++;
-                retardo -= 50;
-                level++;
+                cadena = string.Empty;
+                usuario = string.Empty;
 
-            }
-            else
-            {
-                MessageBox.Show("Has fallado! Inténtalo otra vez");
-                start.Enabled = true;
-                button5.Enabled = false;
-                if (level > 1)
+                //lbl_Nivel.Text = "Nivel: " + level;
+                start.Enabled = false;
+                Random rnd = new Random();
+                int num = 0;
+
+                for (int i = 0; i < dificultad; i++)
                 {
-                    dificultad--;
-                    retardo += 50;
-                    level--;
+                    num = rnd.Next(0, 4);
+                    cadena += num;
+
+                    switch (num)
+                    {
+                        case 0:
+                            Colorear(Color.DarkRed, Color.LightSalmon, button1);
+                            break;
+                        case 1:
+                            Colorear(Color.DarkGreen, Color.LightGreen, button2);
+                            break;
+                        case 2:
+                            Colorear(Color.MidnightBlue, Color.LightBlue, button3);
+                            break;
+                        case 3:
+                            Colorear(Color.Gold, Color.Khaki, button4);
+                            break;
+                    }
                 }
-                               
+
+                Acertar();
             }
 
-        }
+            private void Colorear(Color origen, Color nuevo, Button nBut)
+            {
+                Thread.Sleep(100);
+                nBut.BackColor = nuevo;
+                Application.DoEvents();
+                Thread.Sleep(retardo);
+                nBut.BackColor = origen;
+                Application.DoEvents();
+            }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            MessageBox.Show("Programa creado por José Luis del Pino Gallardo \nV1.0.1");
-        }
+            private void Acertar()
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+
+            private void DesactivarBot()
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+        #endregion
+
+        #region Eventos
+                private void button1_Click(object sender, EventArgs e)
+                {
+                    usuario += 0;
+
+                }
+
+                private void button2_Click(object sender, EventArgs e)
+                {
+                    usuario += 1;
+
+                }
+
+                private void button3_Click(object sender, EventArgs e)
+                {
+                    usuario += 2;
+
+                }
+
+                private void button4_Click(object sender, EventArgs e)
+                {
+                    usuario += 3;
+
+                }
+
+                private void start_Click(object sender, EventArgs e)
+                {
+                    Generar();
+                }
+
+                private void button5_Click(object sender, EventArgs e)
+                {
+                    DesactivarBot();
+                    if (cadena == usuario)
+                    {
+                        MessageBox.Show("Has acertado ¡Enhorabuena!");
+                        start.Enabled = true;
+                        button5.Enabled = false;
+                        dificultad++;
+                        retardo -= 50;
+                        level++;
+                        lbl_Nivel.Text = "Nivel " + level;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("¡Has fallado! Inténtalo otra vez...");
+                        start.Enabled = true;
+                        button5.Enabled = false;
+                        if (level > 1)
+                        {
+                            dificultad--;
+                            retardo += 50;
+                            level--;
+                            lbl_Nivel.Text = "Nivel " + level;
+                        }
+
+                    }
+
+                }
+
+                private void reiniciarToolStripMenuItem_Click(object sender, EventArgs e)
+                {
+                    Application.Restart();
+                }
+
+                private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+                {
+                    MessageBox.Show("\tAutor: José Luis del Pino Gallardo \n\tVersion: V1.2\n\tFecha: 05/11/2015\n\tTwitter: @JoseluLittle");
+
+                }
+
+                private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+                {
+                    Application.Exit();
+                }
+                private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+                {
+                    MessageBox.Show("Programa inspirado en el clásico juego \"Simon Dice...\"\n\nAl pulsar \"Comenzar\" se encenderan las baldosas de colores en un orden aleatorio, intenta recordarlo porque cuando termine será tu turno de introducir el mismo patrón. \n\nSi lo introduces correctamente avanzarás a un nivel superior más difícil. Si fallas volverás al nivel anterior... \n\n\t\t\t¡Buena Suerte!");
+                }
+            #endregion
+
+               
+
+
+
+
+        
     }
 }
